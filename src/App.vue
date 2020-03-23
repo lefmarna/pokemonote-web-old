3615d1d5fa61c6be3a8328c5d9b1b0542c61c77f
lefmarna <panume5ya@gmail.com>
2020-03-23 14:08:29 +0900

    初期設定

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/calc-stats">ステータス計算機</router-link>
     </div>
     <router-view />
   </div>
