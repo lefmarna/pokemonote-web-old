b800a1f6252aaace7452db74908f6aee0254a206
lefmarna <panume5ya@gmail.com>
2020-11-05 11:24:48 +0900

    BootstrapからVuetifyへの乗り換え

--- src/main.js
+++ src/main.js
@@ -3,20 +3,17 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import BootstrapVue from "bootstrap-vue";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
-Vue.use(BootstrapVue);
-
 // Google AdSense ディスプレイ広告のみ利用する設定
 import Adsense from "vue-google-adsense/dist/Adsense.min.js";
 Vue.use(require("vue-script2"));
 Vue.use(Adsense);
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
