b147c8e1e0728ea46938e23724818f9efbfd2a6b
lefmarna <panume5ya@gmail.com>
2020-11-05 12:06:06 +0900

    ポケモンの更新はVuexで行う形に変更（初期ポケモンもランドロス(霊獣)へと変更した）

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,31 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    currentPokemon: {
+      no: 645,
+      name: "ランドロス(霊獣)",
+      form: "れいじゅうフォルム",
+      evolutions: [],
+      types: ["じめん", "ひこう"],
+      abilities: ["いかく"],
+      hiddenAbilities: [],
+      stats: {
+        hp: 89,
+        attack: 145,
+        defence: 90,
+        spAttack: 105,
+        spDefence: 80,
+        speed: 91,
+      },
+    },
+  },
+  getters: {
+    currentPokemon: (state) => state.currentPokemon,
+  },
+  mutations: {
+    updateCurrentPokemon(state, selectedPokemon) {
+      state.currentPokemon = selectedPokemon;
+    },
+  },
 });
