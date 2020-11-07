1e5e570d50a086e7dff51f2450ad00f7911e1403
lefmarna <panume5ya@gmail.com>
2020-11-07 18:42:11 +0900

    ステータス計算機に広告を掲載

--- src/views/CalcStats.vue
+++ src/views/CalcStats.vue
@@ -733,6 +733,10 @@
         </v-container>
       </v-col>
     </v-row>
+    <Adsense
+      data-ad-client="ca-pub-3240586325286249"
+      data-ad-slot="3353369882"
+    ></Adsense>
   </v-container>
 </template>
 
