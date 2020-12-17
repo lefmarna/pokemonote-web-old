88e01fabd826f51d4d819227c85e85a1f6f7f0eb
lefmarna <panume5ya@gmail.com>
2020-12-17 16:02:13 +0900

    ポケモンフィルターをTypescriptに対応

--- src/components/SearchPokemon.vue
+++ src/components/SearchPokemon.vue
@@ -13,37 +13,39 @@
   ></v-autocomplete>
 </template>
 
-<script>
+<script lang="ts">
+import Vue from "vue";
 import PokemonData from "@/components/pokemon_data.json";
 
-export default {
-  data: () => ({
-    pokemonList: PokemonData, // ポケモンのデータはjsonファイルにまとめてあるため、そちらから取得する
-  }),
+export default Vue.extend({
   computed: {
+    pokemonList() {
+      return PokemonData; // ポケモンのデータはjsonファイルにまとめてあるため、そちらから取得する
+    },
     currentPokemon: {
       get() {
         return this.$store.getters.currentPokemon;
       },
-      set(selectedPokemon) {
+      set(selectedPokemon): void {
         this.$store.commit("updateCurrentPokemon", selectedPokemon);
-        document.activeElement.blur(); // ポケモンを更新後、フォーカスを外す
+        (document.activeElement as HTMLElement).blur(); // ポケモンを更新後、フォーカスを外す
       },
     },
   },
   methods: {
     // 元々の検索ワード、または、カタカナに変換後の検索ワードと一致する結果を返すフィルター
-    hiraganaToKatakana(item, queryText) {
+    hiraganaToKatakana(item: { name: string }, queryText: string): boolean {
+      const itemName = item.name;
       // ひらがなをカタカナに置き換える
       const katakana = queryText.replace(/[ぁ-ん]/g, (t) =>
         String.fromCharCode(t.charCodeAt(0) + 96)
       );
       // 複数条件を指定するケースでは、"test"を使ったほうがパフォーマンスが良いと思われたが、動的な正規表現はどうやら重い？らしく、"無難にindexOf"で検索するようにした。
       return (
-        (item.name || "").indexOf(queryText) > -1 ||
-        (item.name || "").indexOf(katakana) > -1
+        (itemName || "").indexOf(queryText) > -1 ||
+        (itemName || "").indexOf(katakana) > -1
       );
     },
   },
-};
+});
 </script>
