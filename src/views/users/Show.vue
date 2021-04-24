fafa88c690f5ea333d496e5f43b6914b72ed17b9
lefmarna <panume5ya@gmail.com>
2021-04-24 19:44:22 +0900

    ユーザー詳細ページのデザインを一部変更

--- src/views/users/Show.vue
+++ src/views/users/Show.vue
@@ -3,10 +3,10 @@
     <p>ID：{{ id }}</p>
     <p>ユーザー名：{{ user.username }}</p>
     <p>表示名：{{ user.nickname }}</p>
-    <!-- <v-avatar v-if="user.image" size="36px">
+    <v-avatar v-if="user.image" size="36px">
       <img alt="アイコン" :src="user.image" />
-    </v-avatar> -->
-    <PokemonTable title="ポケモン" :pokemons="pokemons" />
+    </v-avatar>
+    <PokemonTable :title="`${user.nickname}さんの投稿`" :pokemons="pokemons" />
   </div>
   <div v-else>ユーザー情報を読み込んでいます...</div>
 </template>
@@ -35,6 +35,7 @@ export default Vue.extend({
           .get(`/users/${this.id}`)
           .then((response) => {
             this.user = JSON.parse(response.data.user);
+            this.user.image = response.data.image;
             this.pokemons = JSON.parse(response.data.pokemons);
           })
           .catch((error) => {
