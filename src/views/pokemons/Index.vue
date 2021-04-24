4230f72a0fab4e23d5ce288ea4f3ede3247e2b70
lefmarna <panume5ya@gmail.com>
2021-04-24 13:30:39 +0900

    ポケモンの一覧ページを作成

--- src/views/pokemons/Index.vue
+++ src/views/pokemons/Index.vue
@@ -12,10 +12,16 @@
       ></v-text-field>
     </v-card-title>
     <v-data-table :headers="headers" :items="pokemons" :search="search">
-      <!-- v-slotを使うことで、nameにリンクを設定する -->
+      <!-- v-slotを使うことで、ポケモン名にリンクを設定する -->
       <template v-slot:[`item.name`]="{ item }">
         <router-link :to="`/pokemons/${item.id}`">
           {{ item.name }}
+        </router-link>
+      </template>
+      <!-- v-slotを使うことで、ユーザー名にリンクを設定する -->
+      <template v-slot:[`item.user.nickname`]="{ item }">
+        <router-link :to="`/users/${item.user_id}`">
+          {{ item.user.nickname }}
         </router-link>
       </template>
     </v-data-table>
@@ -31,17 +37,57 @@ export default Vue.extend({
     pokemons: [],
     search: "",
     headers: [
-      { text: "ポケモン名", sortable: false, value: "id" },
-      { text: "性格", sortable: false, value: "username" },
-      { text: "ステータス", sortable: false, value: "nickname" },
-      { text: "投稿者", sortable: false, value: "nickname" },
+      { text: "ポケモン名", sortable: false, value: "name" },
+      { text: "レベル", sortable: false, value: "lv" },
+      { text: "性格", sortable: false, value: "nature" },
+      { text: "ステータス", sortable: false, value: "stats" },
+      { text: "投稿者", sortable: false, value: "user.nickname" },
     ],
   }),
   created() {
     axios
       .get("/pokemons")
       .then((response) => {
-        this.pokemons = response.data;
+        // 努力値と実数値は1行にまとめる
+        this.pokemons = response.data.map((pokemon: any) => {
+          let result = "";
+
+          // 努力値
+          const effortValues = [
+            pokemon.hp_ev,
+            pokemon.attack_ev,
+            pokemon.defence_ev,
+            pokemon.sp_attack_ev,
+            pokemon.sp_defence_ev,
+            pokemon.speed_ev,
+          ];
+
+          // 実数値
+          const realNumbers = [
+            pokemon.hp,
+            pokemon.attack,
+            pokemon.defence,
+            pokemon.sp_attack,
+            pokemon.sp_defence,
+            pokemon.speed,
+          ];
+
+          for (let i = 0, len = effortValues.length; i < len; i++) {
+            // HPの前にはハイフンをつけない
+            if (i > 0) {
+              result += "-";
+            }
+            // 実数値を代入
+            result += String(realNumbers[i]);
+            // 努力値が振られていれば()で囲んで代入
+            if (effortValues[i] > 0 && realNumbers[i] != "*") {
+              result += `(${effortValues[i]})`;
+            }
+          }
+          // statsというキーを作り、結果を代入
+          pokemon.stats = result;
+          return pokemon;
+        });
       })
       .catch((error) => {
         console.log(error);
