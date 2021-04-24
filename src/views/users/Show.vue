23526151cf24e8d968d9e7323a3b890dc0bb1016
lefmarna <panume5ya@gmail.com>
2021-04-24 17:30:53 +0900

    ユーザー詳細ページに投稿したポケモンの情報を出力

--- src/views/users/Show.vue
+++ src/views/users/Show.vue
@@ -3,17 +3,10 @@
     <p>ID：{{ id }}</p>
     <p>ユーザー名：{{ user.username }}</p>
     <p>表示名：{{ user.nickname }}</p>
-    <v-avatar v-if="user.image" size="36px">
+    <!-- <v-avatar v-if="user.image" size="36px">
       <img alt="アイコン" :src="user.image" />
-    </v-avatar>
-    <v-data-table :headers="headers" :items="pokemons" :search="search">
-      <!-- v-slotを使うことで、nicknameにリンクを設定する -->
-      <!-- <template v-slot:[`item.lv`]="{ item }">
-        <router-link :to="`/pokemons/${item.id}`">
-          {{ item.lv }}
-        </router-link>
-      </template> -->
-    </v-data-table>
+    </v-avatar> -->
+    <PokemonTable title="ポケモン" :pokemons="pokemons" />
   </div>
   <div v-else>ユーザー情報を読み込んでいます...</div>
 </template>
@@ -21,20 +14,17 @@
 <script lang="ts">
 import Vue from "vue";
 import axios from "axios";
+import PokemonTable from "@/components/PokemonTable.vue";
 
 export default Vue.extend({
   data: () => ({
     user: [],
     pokemons: [],
-    search: "",
-    headers: [
-      { text: "ポケモン名", sortable: false, value: "id" },
-      { text: "性格", sortable: false, value: "hp_iv" },
-      { text: "ステータス", sortable: false, value: "hp_ev" },
-      { text: "投稿者", sortable: false, value: "speed_iv" },
-    ],
   }),
   props: { id: Number },
+  components: {
+    PokemonTable,
+  },
   // コンポーネントの更新ではライフサイクルの初期化を行わないため、createdではなくwatchで監視している
   watch: {
     id: {
@@ -44,8 +34,8 @@ export default Vue.extend({
         axios
           .get(`/users/${this.id}`)
           .then((response) => {
-            this.user = response.data;
-            this.pokemons = response.data.pokemons;
+            this.user = JSON.parse(response.data.user);
+            this.pokemons = JSON.parse(response.data.pokemons);
           })
           .catch((error) => {
             console.log(error);
