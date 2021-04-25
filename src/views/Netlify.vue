a3de4d6a45bba03f231d04ece00ea685a09acf61
lefmarna <panume5ya@gmail.com>
2021-04-25 22:54:59 +0900

    Netlify Formsの有効化

--- src/views/Netlify.vue
+++ src/views/Netlify.vue
@@ -1,5 +1,5 @@
 <template>
-  <form name="contact" method="POST" data-netlify="true">
+  <form name="contact" method="POST" netlify>
     <p>
       <input type="hidden" name="form-name" value="contact" />
       <label>Your Name: <input type="text" name="name" /></label>
