1858fa756e137e2e6369199d78d47062bb9529a1
lefmarna <panume5ya@gmail.com>
2021-05-09 23:56:01 +0900

    ポケモン検索のコンポーネントを、コンポーネント内で処理する形から、親コンポーネントに処理を渡す形に変更

--- src/components/molecules/SearchPokemon.vue
+++ src/components/molecules/SearchPokemon.vue
@@ -4,12 +4,13 @@
     item-text="name"
     label="ポケモン名"
     :filter="filterForSearch"
-    v-model="currentPokemon"
     no-data-text="ポケモンが見つかりません。"
     class="mt-3"
     clearable
     auto-select-first
     return-object
+    :value="currentPokemon"
+    @input="updateCurrentPokemon"
   ></v-autocomplete>
 </template>
 
@@ -17,8 +18,27 @@
 import Vue from "vue";
 import filterForSearch from "@/mixins/filterForSearch";
 
+export type DataType = {
+  currentPokemon: {
+    no?: number;
+    name?: string;
+    form?: string;
+    ranks?: string[];
+    evolutions?: number[];
+    types?: string[];
+    abilities?: string[];
+    hiddenAbilities?: string[];
+    stats?: {
+      [key: string]: number;
+    };
+  };
+};
+
 export default Vue.extend({
   mixins: [filterForSearch],
+  data: (): DataType => ({
+    currentPokemon: {},
+  }),
   computed: {
     pokemonData(): {
       no: number;
@@ -32,20 +52,16 @@ export default Vue.extend({
       stats: {
         [key: string]: number;
       };
-    } {
+    }[] {
       return this.$store.getters.pokemonData;
     },
-    currentPokemon: {
-      get() {
-        return this.$store.getters.currentPokemon;
-      },
-      set(selectedPokemon): void {
-        // 全消し(clearable)を利用する場合、ポケモンの情報がなくなってしまうと不具合が発生するため、入力情報がない場合は更新させないようにした
-        if (selectedPokemon) {
-          this.$store.commit("updateCurrentPokemon", selectedPokemon);
-        }
-        (document.activeElement as HTMLElement).blur(); // ポケモンを更新後、フォーカスを外す
-      },
+  },
+  methods: {
+    updateCurrentPokemon($event) {
+      if ($event) {
+        this.$emit("update:currentPokemon", $event);
+      }
+      (document.activeElement as HTMLElement).blur(); // ポケモンを更新後、フォーカスを外す
     },
   },
 });
