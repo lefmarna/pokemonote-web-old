da78e685190508abb5710722bdea075cb92e5c6b
lefmarna <panume5ya@gmail.com>
2021-05-19 23:25:12 +0900

    テストコードを記述（実数値の更新）

--- tests/unit/CalcStats.spec.ts
+++ tests/unit/CalcStats.spec.ts
@@ -111,8 +111,6 @@ describe("CalcButtonの正常系テスト", () => {
   it("propsが渡せていること", () => {
     expect(wrapper.vm.currentPokemon).toBe(currentPokemon);
     expect(wrapper.vm.currentNature).toBe(currentNature);
-    // await wrapper.setProps({ lv: 100 });
-    // await console.log(wrapper.vm.lv);
     expect(wrapper.vm.lv).toBe(lv);
     expect(wrapper.vm.stats).toBe(stats);
   });
@@ -140,6 +138,180 @@ describe("CalcButtonの正常系テスト", () => {
   });
 
   it("めざパが正しく反映されていること", () => {
+    expect(wrapper.html()).toContain("めざパ：ひこう");
+  });
+
+  it("「ＨＰ」の実数値を変更したとき、各種値が正しく更新されること", async () => {
+    await wrapper.setData({ hp: 160 });
+    // 個体値
+    expect(wrapper.vm.stats[0].individualValue).toBe(25);
+    expect(wrapper.vm.stats[1].individualValue).toBe(15);
+    expect(wrapper.vm.stats[2].individualValue).toBe(5);
+    expect(wrapper.vm.stats[3].individualValue).toBe(30);
+    expect(wrapper.vm.stats[4].individualValue).toBe(20);
+    expect(wrapper.vm.stats[5].individualValue).toBe(10);
+    // 努力値
+    expect(wrapper.vm.stats[0].effortValue).toBe(76);
+    expect(wrapper.vm.stats[1].effortValue).toBe(20);
+    expect(wrapper.vm.stats[2].effortValue).toBe(40);
+    expect(wrapper.vm.stats[3].effortValue).toBe(60);
+    expect(wrapper.vm.stats[4].effortValue).toBe(80);
+    expect(wrapper.vm.stats[5].effortValue).toBe(100);
+    // ステータスの合計
+    expect(wrapper.vm.totalBaseStats).toBe(341);
+    expect(wrapper.vm.totalIv).toBe(105);
+    expect(wrapper.vm.totalEv).toBe(376);
+    expect(wrapper.vm.totalStats).toBe(526);
+    // 耐久指数
+    expect(wrapper.html()).toContain("総合：27360");
+    expect(wrapper.html()).toContain("物理：11520");
+    expect(wrapper.html()).toContain("特殊：15840");
+    // その他
+    expect(wrapper.html()).toContain("めざパ：ひこう");
+  });
+
+  it("「こうげき」の実数値を変更したとき、各種値が正しく更新されること", async () => {
+    await wrapper.setData({ attack: 70 });
+    // 個体値
+    expect(wrapper.vm.stats[0].individualValue).toBe(25);
+    expect(wrapper.vm.stats[1].individualValue).toBe(15);
+    expect(wrapper.vm.stats[2].individualValue).toBe(5);
+    expect(wrapper.vm.stats[3].individualValue).toBe(30);
+    expect(wrapper.vm.stats[4].individualValue).toBe(20);
+    expect(wrapper.vm.stats[5].individualValue).toBe(10);
+    // 努力値
+    expect(wrapper.vm.stats[0].effortValue).toBe(76);
+    expect(wrapper.vm.stats[1].effortValue).toBe(108);
+    expect(wrapper.vm.stats[2].effortValue).toBe(40);
+    expect(wrapper.vm.stats[3].effortValue).toBe(60);
+    expect(wrapper.vm.stats[4].effortValue).toBe(80);
+    expect(wrapper.vm.stats[5].effortValue).toBe(100);
+    // ステータスの合計
+    expect(wrapper.vm.totalBaseStats).toBe(341);
+    expect(wrapper.vm.totalIv).toBe(105);
+    expect(wrapper.vm.totalEv).toBe(464);
+    expect(wrapper.vm.totalStats).toBe(536);
+    // 耐久指数
+    expect(wrapper.html()).toContain("総合：27360");
+    expect(wrapper.html()).toContain("物理：11520");
+    expect(wrapper.html()).toContain("特殊：15840");
+    // その他
+    expect(wrapper.html()).toContain("めざパ：ひこう");
+  });
+
+  it("「ぼうぎょ」の実数値を変更したとき、各種値が正しく更新されること", async () => {
+    await wrapper.setData({ defence: 70 });
+    // 個体値
+    expect(wrapper.vm.stats[0].individualValue).toBe(25);
+    expect(wrapper.vm.stats[1].individualValue).toBe(15);
+    expect(wrapper.vm.stats[2].individualValue).toBe(5);
+    expect(wrapper.vm.stats[3].individualValue).toBe(30);
+    expect(wrapper.vm.stats[4].individualValue).toBe(20);
+    expect(wrapper.vm.stats[5].individualValue).toBe(10);
+    // 努力値
+    expect(wrapper.vm.stats[0].effortValue).toBe(76);
+    expect(wrapper.vm.stats[1].effortValue).toBe(108);
+    expect(wrapper.vm.stats[2].effortValue).toBe(20);
+    expect(wrapper.vm.stats[3].effortValue).toBe(60);
+    expect(wrapper.vm.stats[4].effortValue).toBe(80);
+    expect(wrapper.vm.stats[5].effortValue).toBe(100);
+    // ステータスの合計
+    expect(wrapper.vm.totalBaseStats).toBe(341);
+    expect(wrapper.vm.totalIv).toBe(105);
+    expect(wrapper.vm.totalEv).toBe(444);
+    expect(wrapper.vm.totalStats).toBe(534);
+    // 耐久指数
+    expect(wrapper.html()).toContain("総合：27040");
+    expect(wrapper.html()).toContain("物理：11200");
+    expect(wrapper.html()).toContain("特殊：15840");
+    // その他
+    expect(wrapper.html()).toContain("めざパ：ひこう");
+  });
+
+  it("「とくこう」の実数値を変更したとき、各種値が正しく更新されること", async () => {
+    await wrapper.setData({ spAttack: 85 });
+    // 個体値
+    expect(wrapper.vm.stats[0].individualValue).toBe(25);
+    expect(wrapper.vm.stats[1].individualValue).toBe(15);
+    expect(wrapper.vm.stats[2].individualValue).toBe(5);
+    expect(wrapper.vm.stats[3].individualValue).toBe(30);
+    expect(wrapper.vm.stats[4].individualValue).toBe(20);
+    expect(wrapper.vm.stats[5].individualValue).toBe(10);
+    // 努力値
+    expect(wrapper.vm.stats[0].effortValue).toBe(76);
+    expect(wrapper.vm.stats[1].effortValue).toBe(108);
+    expect(wrapper.vm.stats[2].effortValue).toBe(20);
+    expect(wrapper.vm.stats[3].effortValue).toBe(16);
+    expect(wrapper.vm.stats[4].effortValue).toBe(80);
+    expect(wrapper.vm.stats[5].effortValue).toBe(100);
+    // ステータスの合計
+    expect(wrapper.vm.totalBaseStats).toBe(341);
+    expect(wrapper.vm.totalIv).toBe(105);
+    expect(wrapper.vm.totalEv).toBe(400);
+    expect(wrapper.vm.totalStats).toBe(529);
+    // 耐久指数
+    expect(wrapper.html()).toContain("総合：27040");
+    expect(wrapper.html()).toContain("物理：11200");
+    expect(wrapper.html()).toContain("特殊：15840");
+    // その他
+    expect(wrapper.html()).toContain("めざパ：ひこう");
+  });
+
+  it("「とくぼう」の実数値を変更したとき、各種値が正しく更新されること", async () => {
+    await wrapper.setData({ spDefence: 105 });
+    // 個体値
+    expect(wrapper.vm.stats[0].individualValue).toBe(25);
+    expect(wrapper.vm.stats[1].individualValue).toBe(15);
+    expect(wrapper.vm.stats[2].individualValue).toBe(5);
+    expect(wrapper.vm.stats[3].individualValue).toBe(30);
+    expect(wrapper.vm.stats[4].individualValue).toBe(20);
+    expect(wrapper.vm.stats[5].individualValue).toBe(10);
+    // 努力値
+    expect(wrapper.vm.stats[0].effortValue).toBe(76);
+    expect(wrapper.vm.stats[1].effortValue).toBe(108);
+    expect(wrapper.vm.stats[2].effortValue).toBe(20);
+    expect(wrapper.vm.stats[3].effortValue).toBe(16);
+    expect(wrapper.vm.stats[4].effortValue).toBe(128);
+    expect(wrapper.vm.stats[5].effortValue).toBe(100);
+    // ステータスの合計
+    expect(wrapper.vm.totalBaseStats).toBe(341);
+    expect(wrapper.vm.totalIv).toBe(105);
+    expect(wrapper.vm.totalEv).toBe(448);
+    expect(wrapper.vm.totalStats).toBe(535);
+    // 耐久指数
+    expect(wrapper.html()).toContain("総合：28000");
+    expect(wrapper.html()).toContain("物理：11200");
+    expect(wrapper.html()).toContain("特殊：16800");
+    // その他
+    expect(wrapper.html()).toContain("めざパ：ひこう");
+  });
+
+  it("「すばやさ」の実数値を変更したとき、各種値が正しく更新されること", async () => {
+    await wrapper.setData({ speed: 39 });
+    // 個体値
+    expect(wrapper.vm.stats[0].individualValue).toBe(25);
+    expect(wrapper.vm.stats[1].individualValue).toBe(15);
+    expect(wrapper.vm.stats[2].individualValue).toBe(5);
+    expect(wrapper.vm.stats[3].individualValue).toBe(30);
+    expect(wrapper.vm.stats[4].individualValue).toBe(20);
+    expect(wrapper.vm.stats[5].individualValue).toBe(10);
+    // 努力値
+    expect(wrapper.vm.stats[0].effortValue).toBe(76);
+    expect(wrapper.vm.stats[1].effortValue).toBe(108);
+    expect(wrapper.vm.stats[2].effortValue).toBe(20);
+    expect(wrapper.vm.stats[3].effortValue).toBe(16);
+    expect(wrapper.vm.stats[4].effortValue).toBe(128);
+    expect(wrapper.vm.stats[5].effortValue).toBe(48);
+    // ステータスの合計
+    expect(wrapper.vm.totalBaseStats).toBe(341);
+    expect(wrapper.vm.totalIv).toBe(105);
+    expect(wrapper.vm.totalEv).toBe(396);
+    expect(wrapper.vm.totalStats).toBe(529);
+    // 耐久指数
+    expect(wrapper.html()).toContain("総合：28000");
+    expect(wrapper.html()).toContain("物理：11200");
+    expect(wrapper.html()).toContain("特殊：16800");
+    // その他
     expect(wrapper.html()).toContain("めざパ：ひこう");
   });
 });
