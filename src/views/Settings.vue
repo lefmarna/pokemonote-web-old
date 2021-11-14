fed99fcd317e480c7441d2b3eeaf8ec72dcb65cb
lefmarna <panume5ya@gmail.com>
2021-11-14 11:18:02 +0900

    退会ボタンの作成

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,3 +1,30 @@
 <template>
-  <div>準備中</div>
+  <v-btn color="error" elevation="2" @click="unsubscribe">退会</v-btn>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+import router from "@/router";
+import axios from "axios";
+
+export default Vue.extend({
+  computed: {
+    authUserName() {
+      return this.$store.getters.authUser.username;
+    },
+  },
+  methods: {
+    unsubscribe() {
+      axios.delete(`/users/${this.authUserName}`).then(() => {
+        // Vuexから認証情報を削除する
+        this.$store.commit("updateAuthUser", {
+          username: "",
+          nickname: "",
+        });
+        // ログインページにリダイレクトする
+        router.replace("/login");
+      });
+    },
+  },
+});
+</script>
