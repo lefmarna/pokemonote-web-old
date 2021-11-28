31113f93c76d0c484f3c3c34b908405e2cad94b0
lefmarna <panume5ya@gmail.com>
2021-11-28 19:47:37 +0900

    メール確認のページを仮作成

--- src/views/email/Verify.vue
+++ src/views/email/Verify.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>メールを確認してください。</div>
+  <Form title="メール確認" buttonText="メールを再送信する" @submit="resend">
+    メールをご確認ください。
+    もし確認用メールが送信されていない場合は、下記をクリックしてください。
+  </Form>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+import axios from "axios";
+import Vue from "vue";
+import Form from "@/components/templates/Form.vue";
+
+export default Vue.extend({
+  components: {
+    Form,
+  },
+  methods: {
+    resend(): void {
+      axios
+        .post("/email/resend")
+        .then(() => {
+          alert("メールを再送信しました。");
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+        });
+    },
+  },
+});
+</script>
