b193ab48e950b60b769339fd6b9201f7ae21c4d9
lefmarna <panume5ya@gmail.com>
2021-12-04 16:29:56 +0900

    種族値ランキングをCompositionAPIに対応

--- src/views/BaseStatsRanking.vue
+++ src/views/BaseStatsRanking.vue
@@ -49,68 +49,68 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import { computed, defineComponent, reactive } from "@vue/composition-api";
+import store from "@/store";
 
-export interface DataType {
-  displayRankPokemons: { [key: string]: boolean };
-  removeStats: { [key: string]: boolean };
-}
-
-export default Vue.extend({
-  data: (): DataType => ({
+export default defineComponent({
+  setup() {
     // 【特別なポケモンを表示する】
-    displayRankPokemons: {
+    const displayRankPokemons = reactive({
       legendary: false,
       mythical: false,
       mega: false,
       NotInPokedex: false,
-    },
+    });
+
     // 【除外するステータス】
-    removeStats: {
+    const removeStats = reactive({
       attack: false,
       spAttack: false,
       speed: false,
-    },
-  }),
-  computed: {
-    pokemonList(): {
-      no: number;
-      name: string;
-      form: string;
-      ranks: string[];
-      evolutions: number[];
-      types: string[];
-      abilities: string[];
-      hiddenAbilities: string[];
-      stats: {
-        [key: string]: number;
-      };
-      total?: number;
-    }[] {
-      return this.$store.getters.pokemonData;
-    },
+    });
+
+    const pokemonList = computed(
+      (): {
+        no: number;
+        name: string;
+        form: string;
+        ranks: string[];
+        evolutions: number[];
+        types: string[];
+        abilities: string[];
+        hiddenAbilities: string[];
+        stats: {
+          [key: string]: number;
+        };
+        total?: number;
+      }[] => {
+        return store.getters.pokemonData;
+      }
+    );
+
     /* ループによる処理を可能にするために、チェックボックスとテキストを対応づけている */
     // 【特別なポケモンを表示する】
-    ranksCheckboxes(): { [key: string]: string }[] {
+    const ranksCheckboxes = computed(() => {
       return [
         { text: "伝説", value: "legendary" },
         { text: "幻", value: "mythical" },
         { text: "メガシンカ", value: "mega" },
         { text: "剣盾に登場しないポケモン", value: "NotInPokedex" },
       ];
-    },
+    });
+
     // 【除外するステータス】
-    statsCheckboxes(): { [key: string]: string }[] {
+    const statsCheckboxes = computed(() => {
       return [
         { text: "攻撃", value: "attack" },
         { text: "特攻", value: "spAttack" },
         { text: "素早さ", value: "speed" },
       ];
-    },
+    });
     /* ここまで */
 
     // v-data-tableに表示させる内容とオプションの設定
-    headers(): { [key: string]: string }[] {
+    const headers = computed(() => {
       const dataTableList = [
         { text: "ポケモン名", value: "name", align: "start", width: "30%" },
         { text: "ＨＰ", value: "stats.hp", align: "end", width: "10%" },
@@ -121,54 +121,52 @@ export default Vue.extend({
         { text: "素早", value: "stats.speed", align: "end", width: "10%" },
         { text: "合計", value: "total", align: "end", width: "10%" },
       ];
-      if (this.removeStats.attack) {
+      if (removeStats.attack) {
         delete dataTableList[2].value;
       }
-      if (this.removeStats.spAttack) {
+      if (removeStats.spAttack) {
         delete dataTableList[4].value;
       }
-      if (this.removeStats.speed) {
+      if (removeStats.speed) {
         delete dataTableList[6].value;
       }
       return dataTableList;
-    },
-    // ステータスの合計(total)を計算して返す
-    pokemonListInTotal() {
+    });
+
+    const pokemonListInTotal = computed(() => {
       // 直接データを書き換えるわけにはいかないので、フィルター用の変数に格納しておく
-      let pokemonList = Array.from(this.pokemonList);
-      // mapメソッドの中ではthisが使えないため、定数に格納しておく
-      const removeStats = this.removeStats;
+      let result = Array.from(pokemonList.value);
 
       // 『メガシンカ』にチェックがついていないときは表示させない
-      if (!this.displayRankPokemons.mega) {
-        pokemonList = pokemonList.filter(
+      if (!displayRankPokemons.mega) {
+        result = result.filter(
           (pokemonData: any) =>
             !pokemonData.ranks.some((rank: string) => rank == "mega")
         );
       }
       // 『伝説』にチェックがついていないときは表示させない
-      if (!this.displayRankPokemons.legendary) {
-        pokemonList = pokemonList.filter(
+      if (!displayRankPokemons.legendary) {
+        result = result.filter(
           (pokemonData: any) =>
             !pokemonData.ranks.some((rank: string) => rank == "legendary")
         );
       }
       // 『幻』にチェックがついていないときは表示させない
-      if (!this.displayRankPokemons.mythical) {
-        pokemonList = pokemonList.filter(
+      if (!displayRankPokemons.mythical) {
+        result = result.filter(
           (pokemonData: any) =>
             !pokemonData.ranks.some((rank: string) => rank == "mythical")
         );
       }
       // 『剣盾に登場しないポケモン』にチェックがついていないときは表示させない
-      if (!this.displayRankPokemons.NotInPokedex) {
-        pokemonList = pokemonList.filter(
+      if (!displayRankPokemons.NotInPokedex) {
+        result = result.filter(
           (pokemonData: any) =>
             !pokemonData.ranks.some((rank: string) => rank == "NotInPokedex")
         );
       }
       // 全てのオブジェクトで合計(total)を計算する
-      pokemonList = pokemonList.map((array: any) => {
+      result = result.map((array: any) => {
         // statsの各数値のみを抽出し、配列に格納する
         const stats = Object.values(array.stats);
         // 配列の要素数が変わってしまうため、後ろから順に削除していく必要がある
@@ -188,22 +186,29 @@ export default Vue.extend({
         }, 0);
         return array;
       });
-      return pokemonList;
-    },
-  },
-  methods: {
+      return result;
+    });
+
     // 【特別なポケモンを表示する】のオンオフを切り替える
-    rankChange(value: string): void {
-      this.$set(
-        this.displayRankPokemons,
-        value,
-        !this.displayRankPokemons[value]
-      );
-    },
+    const rankChange = (value: string): void => {
+      displayRankPokemons[value] = !displayRankPokemons[value];
+    };
+
     // 【除外するステータス】のオンオフを切り替える
-    statsChange(value: string): void {
-      this.$set(this.removeStats, value, !this.removeStats[value]);
-    },
+    const statsChange = (value: string): void => {
+      removeStats[value] = !removeStats[value];
+    };
+
+    return {
+      displayRankPokemons,
+      headers,
+      pokemonListInTotal,
+      ranksCheckboxes,
+      removeStats,
+      statsCheckboxes,
+      rankChange,
+      statsChange,
+    };
   },
 });
 </script>
