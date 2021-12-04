190cc411a990c5a870d089fbcd921493d756d33d
lefmarna <panume5ya@gmail.com>
2021-12-05 00:15:56 +0900

    ステータス計算機をCompositionAPIで実装

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,6 +3,18 @@ export interface Login {
   password: string;
 }
 
+export interface Nature {
+  name: string;
+  stats: {
+    hp: number;
+    attack: number;
+    defence: number;
+    spAttack: number;
+    spDefence: number;
+    speed: number;
+  };
+}
+
 export interface Pokemon {
   id: number;
   lv: number;
@@ -15,6 +27,14 @@ export interface Pokemon {
 
 export interface Ranking {
   name: string;
+}
+
+export interface Stat {
+  en: string;
+  ja: string;
+  abbreviation: string;
+  individualValue: number | null;
+  effortValue: number | null;
 }
 
 export interface User {
--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,9 +1,36 @@
 import { computed } from "@vue/composition-api";
 import store from "@/store";
-import { Ranking, User } from "@/types/index";
+import { Nature, Pokemon, Ranking, Stat, User } from "@/types/index";
 
 export const authUserName = computed((): string => {
   return store.getters.authUser.username;
+});
+
+export const currentNature = computed({
+  get: (): Nature => {
+    return store.getters.currentNature;
+  },
+  set(value): void {
+    store.commit("updateCurrentNature", value);
+  },
+});
+
+export const currentPokemon = computed({
+  get: (): Pokemon => {
+    return store.getters.currentPokemon;
+  },
+  set(value): void {
+    store.commit("updateCurrentPokemon", value);
+  },
+});
+
+export const lv = computed({
+  get(): number | null {
+    return store.getters.lv;
+  },
+  set(value: number | null) {
+    store.commit("updateLv", value);
+  },
 });
 
 export const notice = (): void => {
@@ -12,6 +39,10 @@ export const notice = (): void => {
     store.commit("updateNotice", false);
   }, 2250);
 };
+
+export const stats = computed((): Stat[] => {
+  return store.getters.stats;
+});
 
 export const updateAuthUser = (user: User): void => {
   store.commit("updateAuthUser", user);
