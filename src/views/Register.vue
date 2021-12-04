e1641e926b4382af2e0d34cf7b7a682c63e91a27
lefmarna <panume5ya@gmail.com>
2021-12-04 22:24:08 +0900

    ユーザー登録ページをCompositionAPIで実装

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -42,71 +42,76 @@
 </template>
 
 <script lang="ts">
+import { computed, defineComponent, ref } from "@vue/composition-api";
 import axios from "axios";
 import router from "@/router";
 import Form from "@/components/templates/Form.vue";
 import EmailField from "@/components/molecules/EmailField.vue";
 import PasswordField from "@/components/molecules/PasswordField.vue";
+import store from "@/store";
 
-export interface DataType {
-  image: any;
-  username: string;
-  nickname: string;
-  email: string;
-  password: string;
-  password_confirmation: string;
-  errors: string[];
-  rules: any;
-}
-
-export default {
-  name: "register",
+export default defineComponent({
   components: {
     Form,
     EmailField,
     PasswordField,
   },
-  data: (): DataType => ({
-    image: "",
-    username: "",
-    nickname: "",
-    email: "",
-    password: "",
-    password_confirmation: "",
-    errors: [],
-    rules: {
-      required: (value: any) => !!value || "この項目は必須です",
-      username: (value: string) => {
-        const pattern = /^[a-z\d]{5,15}$/i;
-        return pattern.test(value) || "英数5〜15文字で入力してください。";
-      },
-      nickname: (value: string) => {
-        const length = value.length;
-        return (4 <= length && length <= 50) || "4〜50文字で入力してください。";
-      },
-    },
-  }),
-  methods: {
-    register(): void {
+  setup() {
+    // const image = ref();
+    const username = ref<string>("");
+    const nickname = ref<string>("");
+    const email = ref<string>("");
+    const password = ref<string>("");
+    const password_confirmation = ref<string>("");
+    const errors = ref<string[]>([]);
+
+    const rules = computed(() => {
+      return {
+        required: (value: any) => !!value || "この項目は必須です",
+        username: (value: string) => {
+          const pattern = /^[a-z\d]{5,15}$/i;
+          return pattern.test(value) || "英数5〜15文字で入力してください。";
+        },
+        nickname: (value: string) => {
+          const length = value.length;
+          return (
+            (4 <= length && length <= 50) || "4〜50文字で入力してください。"
+          );
+        },
+      };
+    });
+
+    const register = (): void => {
       // 画像のデータはformDataを介さないと送れない
-      const form: any = document.getElementById("form");
+      const form = document.getElementById("form") as HTMLFormElement;
       const formData = new FormData(form);
       axios
         .post("/register", formData)
         .then((response) => {
           // Vuexに認証情報を保存する
-          this.$store.commit("updateAuthUser", response.data.data);
+          store.commit("updateAuthUser", response.data.data);
           router.push("/");
-          this.$store.dispatch("notice");
+          store.dispatch("notice");
         })
         .catch((error) => {
-          this.errors = [];
+          errors.value = [];
           const errorsMessages: string[] = error.response.data.errors;
           Object.values(errorsMessages).forEach((errorMessages) => {
-            this.errors.push(errorMessages[0]);
+            errors.value.push(errorMessages[0]);
           });
         });
-    },
+    };
+
+    return {
+      username,
+      nickname,
+      email,
+      password,
+      password_confirmation,
+      rules,
+      errors,
+      register,
+    };
   },
-};
+});
 </script>
