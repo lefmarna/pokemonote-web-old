c15cc04edaa886960c81aa4bbeb8ec3477ffbfd1
lefmarna <panume5ya@gmail.com>
2021-12-04 11:44:59 +0900

    ポケモン一覧をCompositionAPIで実装

--- src/views/pokemons/Index.vue
+++ src/views/pokemons/Index.vue
@@ -1,40 +1,43 @@
 <template>
-  <PokemonTable title="ポケモン一覧" :pokemons="pokemons" />
+  <PokemonTable title="ポケモン一覧" :pokemons="filteredPokemons" />
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import { computed, defineComponent, ref } from "@vue/composition-api";
 import axios from "axios";
 import PokemonTable from "@/components/templates/PokemonTable.vue";
+import store from "@/store";
 
-export interface DataType {
-  pokemons: [];
-}
-
-export default Vue.extend({
-  data: (): DataType => ({
-    pokemons: [],
-  }),
+export default defineComponent({
   components: {
     PokemonTable,
   },
-  created() {
+  setup() {
+    const pokemons = ref<[]>([]);
+
+    /**
+     * 自分の投稿したポケモンは表示させない
+     */
+    const filteredPokemons = computed(() => {
+      const authUserName = store.getters.authUser.username;
+      if (!authUserName) return pokemons.value;
+      return pokemons.value.filter(
+        (pokemon: any) => pokemon.user.username != authUserName
+      );
+    });
+
     axios
       .get("/pokemons")
       .then((response) => {
-        let pokemons = response.data.data;
-        const authUserName = this.$store.getters.authUser.username;
-        // 自分の投稿は表示させない
-        if (authUserName) {
-          pokemons = pokemons.filter(
-            (pokemon: any) => pokemon.user.username != authUserName
-          );
-        }
-        this.pokemons = pokemons;
+        pokemons.value = response.data.data;
       })
       .catch((error) => {
         console.log(error);
       });
+
+    return {
+      filteredPokemons,
+    };
   },
 });
 </script>
