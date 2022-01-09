cdf5e1aa0bab70b6cb3f3b41f2b3d0a54baf88dd
lefmarna <panume5ya@gmail.com>
2022-01-09 16:33:21 +0900

    ログイン時のメールアドレスとパスワードを一緒に管理する形で実装

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,14 +5,14 @@
     :errors="errors"
     @submit="login"
   >
-    <EmailField :email.sync="email" />
-    <PasswordField :password.sync="password" />
+    <EmailField :email.sync="loginParams.email" />
+    <PasswordField :password.sync="loginParams.password" />
     <router-link to="/register"> 新規会員登録はこちら</router-link>
   </FormTemplate>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "@vue/composition-api";
+import { defineComponent, reactive, ref } from "@vue/composition-api";
 import axios from "axios";
 import router from "@/router";
 import { FormTemplate } from "@/components/templates";
@@ -32,16 +32,18 @@ export default defineComponent({
     // FIXME 非推奨の書き方なので、Vue3に移行したらuseRouteを使う形で書き直したい
     const route = context.root.$route;
 
-    const email = ref("");
-    const password = ref("");
+    const loginParams = reactive({
+      email: "",
+      password: "",
+    });
     const errors = ref<string[]>();
 
     const login = async (): Promise<void | string[]> => {
       try {
-        const response = await axios.post<{ data: AuthUser }>("/login", {
-          email: email.value,
-          password: password.value,
-        });
+        const response = await axios.post<{ data: AuthUser }>(
+          "/login",
+          loginParams
+        );
 
         // メール認証済でない場合は、メール確認ページへ
         if (!response.data.data.email_verified_at) {
@@ -63,9 +65,8 @@ export default defineComponent({
       }
     };
     return {
-      email,
       errors,
-      password,
+      loginParams,
       login,
     };
   },
