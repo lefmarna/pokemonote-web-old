067b7860d3f517e7f00c608299c5cc9dd92c69e1
lefmarna <panume5ya@gmail.com>
2022-01-15 20:00:37 +0900

    半角整数変換のメソッドをより使いやすい形に修正

--- src/utils/calc.ts
+++ src/utils/calc.ts
@@ -1,16 +1,24 @@
 /**
- * 全角数値を半角数値に変換する。また、数値以外の文字や記号も取り除く。
+ * 正の全角整数を半角整数に変換する。また、数値以外の文字や記号も取り除く。
  */
-export const convertHalfWidthNumber = (str: string): number | null => {
-  const convertNumber = Number(
-    str.replace(/[０-９]/g, (s: string) => {
-      return String.fromCharCode(s.charCodeAt(0) - 65248).replace(
-        /[^0-9]/g,
-        ""
-      );
-    })
-  );
-  if (!convertNumber) return null;
+export const convertToHalfWidthInteger = (
+  str: string,
+  maxNumber: number,
+  allowZero = true
+): number => {
+  // 全角数値を半角数値に変換する
+  const convertValue = str.replace(/[０-９]/g, (s: string) => {
+    return String.fromCharCode(s.charCodeAt(0) - 65248).replace(/[^0-9]/g, "");
+  });
+
+  // 0以上の整数に合致するかどうかを判別する
+  if (!convertValue.match(/^[1-9]\d*$|^0$/)) return null;
+
+  // 値の検証を行う
+  const convertNumber = Number(convertValue);
+  if (!allowZero && convertNumber === 0) return null;
+  if (convertNumber > maxNumber) return maxNumber;
+
   return convertNumber;
 };
 
